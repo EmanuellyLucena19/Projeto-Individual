985cf841db766ea23271d2e223026021cc100e11
EmanuellyLucena19 <emanuelly.lucena@bandtec.com.br>
2021-11-28 23:25:42 -0300

    atualização

--- api-site/src/database/config.js
+++ api-site/src/database/config.js
@@ -1,28 +1,13 @@
 var mysql = require("mysql2");
 var sql = require('mssql');
 
-// CONEXÃO DO SQL SERVER - AZURE (NUVEM)
-var sqlServerConfig = {
-    user: "XXXXX",
-    password: "XXXXX",
-    database: "XXXXX",
-    server: "XXXXX",
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-    },
-    options: {
-        encrypt: true, // for azure
-    }
-}
 
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
     user: "root",
-    database: "acquatec",
-    password: "bandtec",
+    database: "nordeste",
+    password: "Bandtec@1",
 };
 
 function executar(instrucao) {
